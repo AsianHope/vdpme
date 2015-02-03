0fd094663133b3e421b0c64d5c43af496e3ac87d
Lyle Kozloff <lyle@asianhope.org>
2015-02-03 09:35:03 +0000

    templating, authentication

--- mande/views.py
+++ mande/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.views.generic import ListView
 from mande.models import IntakeSurvey
@@ -13,7 +14,35 @@ def index(request):
 
 def student_detail(request, student_id):
     survey = IntakeSurvey.objects.get(pk=student_id)
-    context = {'survey':survey}
+    updates = survey.intakeupdate_set.all().filter().order_by('-date')
+    intake = survey.intakeinternal_set.all().filter().order_by('-enrollment_date')
+    academics = survey.academic_set.all().filter().order_by('-test_date')
+    discipline = survey.discipline_set.all().filter().order_by('-incident_date')
+    if len(updates) > 0:
+        recent_update = updates[0]
+    else:
+        recent_update = "No recent updates"
+
+    if len(intake) > 0:
+        recent_intake = intake[0]
+    else:
+        recent_intake = 'Not enrolled'
+
+    #is this a better approach?
+    try:
+        current_grade = academics.filter(promote=True).latest('test_date')
+    except ObjectDoesNotExist:
+        current_grade = None
+
+    context = {
+        'survey':survey,
+        'updates':updates,
+        'recent_update':recent_update,
+        'recent_intake':recent_intake,
+        'academics':academics,
+        'current_grade':current_grade,
+        'discipline':discipline}
+    print survey.student_id
     return render(request, 'mande/detail.html', context)
 
 def results(request, question_id):
