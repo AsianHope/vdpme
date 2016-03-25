9bc49edbf12a45faa50ba21c46c9955e1cba348d
Solida Sang <ssang@asianhope.org>
2016-03-25 15:31:16 +0700

    changed date in How long in VDP Program to intakeinternal date instead of intakesuvery date

--- mande/views/reports.py
+++ mande/views/reports.py
@@ -733,6 +733,9 @@ def students_promoted_times_report(request,filter_seach=None):
             students_promoted[student] = {
                 'promoted_times':len(Academic.objects.filter(student_id=student,promote=True)),
                 'lastest_promoted_date':Academic.objects.filter(student_id=student,promote=True).latest('test_date').test_date if len(Academic.objects.filter(student_id=student,promote=True)) > 0 else None,
+                'enrolled_date' : student.intakeinternal_set.all().filter().order_by(
+                                                                    '-enrollment_date'
+                                                                )[0].enrollment_date
             }
       return render(request, 'mande/students_promoted_times_report.html',
                             {
